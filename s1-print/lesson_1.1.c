ae968dc0a0cdeb3537784a9c79cd3a2cd129b0c8
Suraj Gopal <surajcancode@gmail.com>
2026-08-30 18:56:08 -0500

    init commit

---
+++ s1-print/lesson_1.1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Suraj\nGopal");
+    return 0;
+}
---
+++ s1-print/lesson_1.2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("name: Suraj Gopal\nage: 41\ngender: male");
+    return 0;
+}
